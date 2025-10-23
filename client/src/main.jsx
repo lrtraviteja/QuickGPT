42cca1c01d4365cffb7e0f6812bdcb6906129ee1
lrtraviteja <lrtraviteja@gmail.com>
2025-10-23 13:52:27 +0530

    Added SideBar component with chat search and navigation features and AppContext for state management

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,8 @@ import { AppContextProvider } from './context/AppContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <AppContextProvider>
       <App />
+    </AppContextProvider>
   </BrowserRouter>,
 )
