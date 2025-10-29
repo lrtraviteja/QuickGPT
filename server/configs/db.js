f9cef845013ddd5fe74aeb7e7e3ef5388a5d8f44
lrtraviteja <lrtraviteja@gmail.com>
2025-10-29 10:43:56 +0530

    feat: initialize server with Express, CORS, and MongoDB connection.

---
+++ server/configs/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB  = async () => {
+    try {
+        mongoose.connection.on('connected', () => console.log('Database connected successfully'))
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickgpt`)
+    } catch (error) {
+        console.log('MongoDB connection failed:', error.message)
+    }
+}
+
+export default connectDB
