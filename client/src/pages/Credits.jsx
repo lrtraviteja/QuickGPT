95070cd8978b495c8e5355a9985eedcc5bf3b616
lrtraviteja <lrtraviteja@gmail.com>
2025-11-01 15:28:06 +0530

    feat: implement dynamic fetching and purchasing of credit plans with error handling in Credits component

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -1,15 +1,40 @@
 import { useEffect, useState } from 'react'
 import { dummyPlans } from '../assets/assets'
 import Loading from './Loading'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 const Credits = () => {
-
+  const {axios, token} = useAppContext()
   const [plans, setPlans] = useState([])
   const [loading, setLoading] = useState(true)
 
-  const fetchPlans = () => {
-    setPlans(dummyPlans)
+  const fetchPlans = async () => {
+    try {
+      const {data} = await axios.get('/api/credit/plan', {headers: {Authorization: token}})
+      if (data.success) {
+        setPlans(data.plans)
+      }
+      else{
+        toast.error(data.message || 'Failed to fetch plans')
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
     setLoading(false)
+  }
+
+  const purchasePlan = async (planId) => {
+    try {
+      const {data} = await axios.post('/api/credit/purchase', {planId}, {headers: {Authorization: token}})
+      if (data.success) {
+        window.location.href = data.url
+      } else {
+        toast.error(data.message)
+      }
+    } catch(error) {
+      toast.error(error.message)
+    }
   }
 
   useEffect(() => {
@@ -49,7 +74,7 @@ const Credits = () => {
                 ))}
               </ul>
             </div>
-            <button className='mt-6 bg-purple-600 hover:bg-purple-700
+            <button onClick={() => toast.promise(purchasePlan(plan._id), {loading: 'Processing...'})} className='mt-6 bg-purple-600 hover:bg-purple-700
             active:bg-purple-800 text-white font-medium py-2 rounded
             transition-colors cursor-pointer'>Buy Now</button>
           </div>
